36069a05d981bbbe05dd08429b8fe459c48058c0
Abdulrahman Elsharqawi <215110068@psu.edu.sa>
2020-01-04 14:52:28 +0300

    add searchbar component

--- src/App.js
+++ src/App.js
@@ -13,26 +13,15 @@ class BooksApp extends Component {
     showSearchPage: false
   }
 
+  back = () => this.setState({ showSearchPage: false })
+
+
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
           <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-
-              </div>
-            </div>
+            <SearchBar goBack={this.back} />
             <div className="search-books-results">
               <ol className="books-grid"></ol>
             </div>
@@ -121,6 +110,26 @@ class Book extends Component {
     )
   }
 }
-class SearchBar extends Component { }
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-books-bar">
+        <button className="close-search" onClick={this.props.goBack}>Close</button>
+        <div className="search-books-input-wrapper">
+          {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+          <input type="text" placeholder="Search by title or author" />
+
+        </div>
+      </div>
+    )
+  }
+}
 
 export default BooksApp
